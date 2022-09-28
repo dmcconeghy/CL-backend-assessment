765d1b88565075f5ba6f0eb12e295381caa756ed
Dave McConeghy <david.mcconeghy@gmail.com>
2022-09-28 10:57:11 -0400

    Adds basic SQLALchmey for DB modeling of user and audio data

--- models.py
+++ models.py
@@ -6,3 +6,27 @@ def connect_db(app):
 
     db.app = app
     db.init_app(app)
+
+class User(db.Model):
+    """ 
+        User Model has two parts:
+            Basic information: id, name, email, address, and image
+            JSON audio data as follows: 
+
+                "{
+                    "ticks": [-96.33, -96.33, -93.47, -89.03999999999999, -84.61, -80.18, -75.75, -71.32, -66.89, -62.46, -58.03, -53.6, -49.17, -44.74, -40.31], 
+                    “selected_tick”: 5, 
+                    "session_id": 3448, 
+                    "step_count": 1
+                }"
+    
+    """
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    address = db.Column(db.String(100), nullable=False)
+    image = db.Column(db.String(100), nullable=False)
+    audio_data = db.Column(db.JSON, nullable=False)
+
+    
