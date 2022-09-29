a530d656e7cb2f0447738f1a1362896c5078d79d
Dave McConeghy <david.mcconeghy@gmail.com>
2022-09-29 17:22:51 -0400

    Implements audio API endpoints including insert/read/update. Adds docstrings for endpoints.

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import os
-from flask import Flask, request, jsonify
+from flask import Flask, request, json
 from flask_debugtoolbar import DebugToolbarExtension
 from sqlalchemy.exc import IntegrityError
-from models import db, connect_db, User
+from models import db, connect_db, User, Audio, Tick
 
 uri = os.environ.get('DATABASE_URL', 'postgresql:///concha_labs')
 
@@ -30,11 +30,22 @@ def home_page():
 
 @app.route('/api/users', methods=['POST'])
 def create_user():
+    """
+        Creates a new user from params: name, email, address, and image. 
+        All fields are required. 
+        Duplicate emails are rejected. 
+        Returns a string of the user's data.
+
+    """
 
     name = request.args.get('name')
     email = request.args.get('email')
     address = request.args.get('address')
     image = request.args.get('image')
+
+    # More complex validation errors could be incorporated here.
+    if not name or not email or not address or not image:
+        return "Missing required field"
 
     try: 
         new_user = User(
@@ -49,22 +60,36 @@ def create_user():
 
         return f"User created {new_user}"
     except IntegrityError:
-        # Adding complexity to our error feedback messages can be integrated with a front end forms and/or
-        # We can treat the user information individually to make sure all are present before checking for potential duplicate emails.  
-        return "A user with that email already exists or you are missing required inputs"
+        # Prevent a DB error from being returned to the user if their email is already in use.
+          
+        return "A user with that email already exists."
     
 @app.route('/api/users/<int:user_id>', methods=['GET'])
 def get_user(user_id):
 
+    """
+        Given a user_id, return the user's basic information.
+    
+    """
+
     user = User.query.get_or_404(user_id)
 
-    # This should be updated to return stringified, ideally with a User class method.  
-    return f"User retrieved: {User.__repr__(user)}" 
+    # This could be returned as formatted JSON instead.
+    # We can also call a helper function to restore the audio data.   
+    return f"User retrieved: {user}" 
 
 @app.route('/api/users/<int:user_id>', methods=['PATCH'])
 def update_user(user_id):
 
+    """
+        Patch route updates user information. 
+        Accepts inputs for name, email, address, or image. 
+    
+    """
+
     user = User.query.get_or_404(user_id)
+
+    # Use the pre-existing field data if a field isn't supplied with an update to its entry. 
 
     user.name = request.args.get('name') or user.name
     user.email = request.args.get('email') or user.email
@@ -73,10 +98,14 @@ def update_user(user_id):
 
     db.session.commit()
 
-    return f"Updated {User.__repr__(user)}"
+    return f"Updated {user}"
     
 @app.route('/api/users/<int:user_id>', methods=['DELETE'])
 def delete_user(user_id):
+    """
+        Deletes the user with the given user_id. 
+    
+    """
 
     user = User.query.get_or_404(user_id)
 
@@ -87,27 +116,169 @@ def delete_user(user_id):
 
 # AUDIO API ROUTES [POST, GET, PATCH]
 
-@app.route('/api/audio/<int:session_id>', methods=['POST'])
-def insert_audio_data(user_id):
+@app.route('/api/audio', methods=['POST'])
+def insert_audio_data():
+    """
+        
+        Creates a new audio entry from params: 
+            user_id, session_id, selected_tick, and step_count. 
 
-    user = User.query.get_or_404(user_id)
+            For example,
 
-    audio = user.audio_data or request.args.get()
+            {   
+                "user_id": 5,
+                ticks": [-96.33, -96.33, -93.47, -89.03999999999999, -84.61, -80.18, -75.75, -71.32, -66.89, -62.46, -58.03, -53.6, -49.17, -44.74, -40.31], 
+                "selected_tick": 5, 
+                "session_id": 3448, 
+                "step_count": 1
+            }
 
-    return f"{audio}" or "No audio data found"
+        All fields are required, including a user_id. 
 
-@app.route('/api/audio/<int:user_id>/audio', methods=['GET'])
-def read_audio_data(user_id):
-    user = User.query.get_or_404(user_id)
+        Additional validation: 
+            “Ticks” must be 15 values and range from -10.0 to -100.0.
+            “Session_id” must be unique.
+            “Step_count” must be unique for every “session_id” and be 0 to 9 in value.
+            “Selected_tick” values must be between 0 and 14.
 
-    return f"{user.audio_data}" or "No audio data found"
+        Ticks are stored in their own, related table. 
 
-@app.route('/api/audio/<int:user_id>/<int:session_id>', methods=['PATCH'])
-def update_audio_data(user_id, session_id):
-    return "Audio data updated"
+        Returns the audio data as a string.
+ 
+    """
+
+    input_string = request.get_json()
+
+    session_id = input_string['session_id']
+    user_id = input_string['user_id']
+    selected_tick = input_string['selected_tick']
+    step_count = input_string['step_count']
+    ticks = input_string['ticks']
+
+    # More complex validation errors could be incorporated here.
+    if not user_id:
+        return "Missing required user_id."
+    if step_count not in range(0, 10):
+        return "Step count must be between 0 and 9"
+    if selected_tick not in range(0, 15):
+        return "Selected tick must be between 0 and 14"
+    if len(ticks) is not 15:
+        return "Ticks must be an array of 15 values"
+    
+    try: 
+        new_audio = Audio(
+            session_id = session_id,
+            user_id = user_id,
+            selected_tick = selected_tick,
+            step_count = step_count
+        )
+
+        try: 
+            for tick in ticks:
+
+                new_Tick = Tick(
+                    session_id = session_id,
+                    tick = float(tick),
+                )
+
+                db.session.add(new_Tick)
+        except IntegrityError:
+           return "Error adding tick data"
+
+        db.session.add(new_audio)
+        
+        db.session.commit()
+
+        return f"Audio data created {new_audio}"
+  
+    except IntegrityError:
+
+        return "Session IDs must be unique."
+
+@app.route('/api/audio/<int:user_id>', methods=['GET'])
+def get_audio_data_by_user(user_id):
+    """
+        Given a user_id, return an array of user audio data sessions. 
+    
+    """
+    
+    audio = Audio.query.filter(Audio.user_id == user_id).all()
+
+    # return f"Audio data for user #{user_id} : {Audio.__repr__(audio)}"
+    return f"Here's the data for user #{user_id}'s sessions: {audio}"
+
+@app.route('/api/audio/session/<int:session_id>', methods=['GET'])
+def get_audio_data_by_session(session_id):
+    """
+        Given a session_id, return the audio data.
+    
+    """
+    
+    audio = Audio.query.get_or_404(session_id)
+
+    return f"Here's the: {audio}"
+
+@app.route('/api/audio/update/<int:session_id>', methods=['PATCH'])
+def update_audio_data(session_id):
+
+    """
+        Patch route updates audio data. 
+        Accepts changes to step_count, selected_tick, or ticks.
+        * Option to change session id will require updating all a session's ticks.
+        * We would also need to check for duplicate session ids before committing. 
+        The ticks array will be overwritten with the newer values.  
+
+        Returns the audio object's repr. 
+    
+    """
+
+    audio = Audio.query.get_or_404(session_id)
+
+    # Check for a valid new step_count value
+    if not request.args.get('step_count'):
+        pass
+    elif int(request.args.get('step_count')) not in range(0, 10):
+        return "Step count must be between 0 and 9"
+    else:
+        audio.step_count = request.args.get('step_count')
+
+    # Check for a valid new selected_tick value
+    if not request.args.get('selected_tick'):
+        pass
+    elif int(request.args.get('selected_tick')) not in range(0, 15):
+        return "Selected tick must be between 0 and 14"
+    else:
+        audio.selected_tick = request.args.get('selected_tick')
+ 
+    # Check to see if new ticks were supplied
+    if not request.args.get('ticks'):
+        pass
+    else:
+
+        updated_ticks_string = request.args.get('ticks')
+        updated_ticks = updated_ticks_string.split(',')
+        updated_ticks = [float(tick) for tick in updated_ticks]
+
+        if len(updated_ticks) is not 15:
+            
+            return f"Ticks must be an array of 15 values"
+         
+
+        original_ticks = Tick.query.filter(Tick.session_id == session_id).all()
+
+        for t in range (0, 15):
+            if updated_ticks[t] > -10.0 or updated_ticks[t] < -100.0:
+                return f"Ticks must be between -10.0 and -100.0"
+
+            original_ticks[t] = updated_ticks[t]
+        
+    db.session.commit()
+
+    return f"Updated {audio}"
+    
     
 # USERS API SEARCH ROUTES [GET by id, name, email, or address]
-# These items will be condensed into a single route with a query string, but I wanted to begin using different routes for each search parameter.
+# These items can be condensed into a single route with a query string.
 
 @app.route('/api/users/search/id', methods=['GET'])
 def search_by_user_id(id):
