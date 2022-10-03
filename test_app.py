5cc20adfd42ed73f5812978c28f26e11d4c07d00
Dave McConeghy <david.mcconeghy@gmail.com>
2022-10-02 23:01:15 -0400

    Initial testing suite for REST endpoints.

---
+++ test_app.py
@@ -0,0 +1,163 @@
+import os
+from unittest import TestCase
+from models import db, User, Audio, Tick
+
+os.environ['DATABASE_URL'] = "postgresql:///concha_labs_test"
+
+from app import app
+
+app.config['TESTING'] = True
+app.config['DEBUG_TB_HOSTS'] = ['dont-show-debug-toolbar']
+
+db.drop_all()
+db.create_all()
+
+class AppTest(TestCase):
+    """
+        Tests the POST route for USERS
+    
+    """
+
+    def setUp(self):
+        """
+            Add sample user
+        """
+        User.query.delete()
+        Audio.query.delete()
+        Tick.query.delete()
+
+    def tearDown(self):
+        """
+            Clean up any fouled transactions
+        """
+        db.session.rollback()
+
+    def test_server_response(self):
+        """
+            Is the server responsing at its root?
+        """
+
+        with app.test_client() as client:
+            resp = client.get('/')
+            html = resp.get_data(as_text=True)
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('Ground Control to Major Tom', html)
+
+    def test_bad_user_id_404(self):
+        """
+            Does the server return a 404 if the user id is not found?
+        """
+        with app.test_client() as client:
+
+            resp = client.get('/api/users/999', follow_redirects=True)
+            # html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 404)
+
+    def test_create_user(self):
+        """
+            Does the server create a new user?
+        """
+        with app.test_client() as client:
+
+            resp = client.post('/api/users?name=betty&email=betty%40email.com&address=9%20ash%20court&image=pictureofme.com/image.jpg', follow_redirects=True)
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('betty', html)
+
+    def test_get_user(self):
+        """
+            Does the server return a user from the db?
+        """
+        with app.test_client() as client:
+
+            resp = client.post('/api/users?name=timon&email=timon%40email.com&address=9%20ash%20court&image=pictureofme.com/image.jpg', follow_redirects=True)
+            self.assertEqual(resp.status_code, 200)
+
+            user_id = User.query.filter_by(name='timon').first().id
+
+            resp = client.get(f"/api/users/{user_id}", follow_redirects=True)
+            html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('timon', html)
+
+    def test_delete_user(self):
+        """
+            If we create a user and then delete them, do they 404?
+
+            Create a user. Check that they exist. 
+            Delete them. Check that they 404.
+        
+        """
+
+        with app.test_client() as client:
+
+            resp = client.post('/api/users?name=pumbaa&email=pumbaa%40email.com&address=9%20ash%20court&image=pictureofme.com/image.jpg', follow_redirects=True)
+            self.assertEqual(resp.status_code, 200)
+
+            user_id = User.query.filter_by(name='pumbaa').first().id
+
+            resp = client.get(f"/api/users/{user_id}", follow_redirects=True)
+            html = resp.get_data(as_text=True)
+            self.assertIn('pumbaa', html)
+
+            resp = client.delete(f"/api/users/{user_id}", follow_redirects=True)
+            self.assertEqual(resp.status_code, 200)
+            html = resp.get_data(as_text=True)
+            self.assertIn(f"User {user_id} deleted", html)
+
+            resp = client.get(f"/api/users/{user_id}", follow_redirects=True)
+            self.assertEqual(resp.status_code, 404)
+            
+    def test_patch_user(self):
+        """
+            Can we update a user's information?
+
+        """
+        with app.test_client() as client:
+
+            resp = client.post('/api/users?name=doctorwho&email=tardis%40email.com&address=TARDIS&image=pictureofme.com/image.jpg', follow_redirects=True)
+            self.assertEqual(resp.status_code, 200)
+
+            user_id = User.query.filter_by(name='doctorwho').first().id
+
+            resp = client.get(f"/api/users/{user_id}", follow_redirects=True)
+            html = resp.get_data(as_text=True)
+            self.assertIn('doctor', html)
+
+            resp = client.patch(f"/api/users/{user_id}?name=themaster", follow_redirects=True)
+            self.assertEqual(resp.status_code, 200)
+            html = resp.get_data(as_text=True)
+            self.assertIn("themaster", html)
+
+    def test_search_queries(self):
+        """
+            Do the endpoints for name/address/email return db results?
+        
+        """
+
+        with app.test_client() as client:
+
+            resp = client.post('/api/users?name=waldo&email=whereami%40email.com&address=Come%20and%20Find%20Me%20Circle&image=pictureofme.com/waldo.jpg', follow_redirects=True)
+            self.assertEqual(resp.status_code, 200)
+
+            # user_id = User.query.filter_by(name='waldo').first().id
+
+            resp = client.get("/api/users/search/name?name=waldo", follow_redirects=True)
+            html = resp.get_data(as_text=True)
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('waldo', html)
+
+            resp = client.get("/api/users/search/address?address=Come%20and%20Find%20Me%20Circle", follow_redirects=True)
+            html = resp.get_data(as_text=True)
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('waldo', html)
+
+            resp = client.get("/api/users/search/email?email=whereami%40email.com", follow_redirects=True)
+            html = resp.get_data(as_text=True)
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('waldo', html)
+           
+
---
+++ test_audio.py
@@ -0,0 +1,129 @@
+import os
+from unittest import TestCase
+from models import db, User, Audio, Tick
+
+os.environ['DATABASE_URL'] = "postgresql:///concha_labs_test"
+
+from app import app
+
+app.config['TESTING'] = True
+app.config['DEBUG_TB_HOSTS'] = ['dont-show-debug-toolbar']
+
+db.drop_all()
+db.create_all()
+
+class AppTest(TestCase):
+    """
+        Tests the POST route for AUDIO
+    
+    """
+
+    def setUp(self):
+        """
+            Add sample user
+        """
+        
+        User.query.delete()
+        Audio.query.delete()
+        Tick.query.delete()
+
+    def tearDown(self):
+        """
+            Clean up any fouled transactions
+        """
+        db.session.rollback()
+  
+    def test_bad_session_id_404(self):
+        """
+            Does the server return a 404 if the audio session_id is not found?
+        """
+        with app.test_client() as client:
+
+            resp = client.get('/api/audio/session/8675309', follow_redirects=True)
+            # html = resp.get_data(as_text=True)
+
+            self.assertEqual(resp.status_code, 404)
+
+    def test_create_audio(self):
+        """
+            Does the server create a new audio data from the POST route?
+            
+            Create a user. Verify their user_id. 
+            Create audio data for that user. Verify that the audio data exists.
+        """
+        with app.test_client() as client:
+            resp = client.post('/api/users?name=Bob%20Marley&email=jamaica%40email.com&address=Sandy%20Beaches&image=pictureofme.com/image.jpg', follow_redirects=True)
+            self.assertEqual(resp.status_code, 200)
+            
+
+            payload = f"{{\"user_id\": 1,\n \"ticks\": [-66.33, -66.33, -63.47, -69.03999999999999, -84.61, -80.18, -75.75, -71.32, -66.89, -62.46, -58.03, -53.6, -49.17, -44.74, -40.31], \"selected_tick\": 5, \"session_id\": 99999, \"step_count\": 0\n}}"
+
+            resp = client.post(f"/api/audio", data = payload, content_type='application/json')
+            # resp = client.get(f"/api/audio/1", content_type='application/json')
+            html = resp.get_data(as_text=True)
+            self.assertIn('99999', html)
+
+    def test_get_audio(self):
+        """
+            Does the server return audio data from the db?
+
+            First create a test user with audio data. 
+            Then fetch the session directly. 
+        """
+        with app.test_client() as client:
+
+            resp = client.post('/api/users?name=Bob%20Marley&email=jamaica%40email.com&address=Sandy%20Beaches&image=pictureofme.com/image.jpg', follow_redirects=True)
+            self.assertEqual(resp.status_code, 200)
+            
+
+            payload = f"{{\"user_id\": 2,\n \"ticks\": [-66.33, -66.33, -63.47, -69.03999999999999, -84.61, -80.18, -75.75, -71.32, -66.89, -62.46, -58.03, -53.6, -49.17, -44.74, -40.31], \"selected_tick\": 5, \"session_id\": 88888, \"step_count\": 0\n}}"
+
+            resp = client.post(f"/api/audio", data = payload, content_type='application/json')
+            self.assertEqual(resp.status_code, 200)
+
+            resp = client.get('/api/audio/session/88888', follow_redirects=True)
+            html = resp.get_data(as_text=True)
+            self.assertEqual(resp.status_code, 200)
+            self.assertIn('Session ID: 88888', html)
+
+    def test_update_audio(self):
+        """
+            With a created user and audio data, can we patch the data?
+
+            Create a user. Create their audio. Check that they exist. 
+            Update their audio data. Check that it was updated.
+            
+        
+        """
+
+        with app.test_client() as client: 
+
+            resp = client.post('/api/users?name=Bob%20Marley&email=jamaica%40email.com&address=Sandy%20Beaches&image=pictureofme.com/image.jpg', follow_redirects=True)
+            self.assertEqual(resp.status_code, 200)
+            
+
+            payload = f"{{\"user_id\": 3,\n \"ticks\": [-66.33, -66.33, -63.47, -69.03999999999999, -84.61, -80.18, -75.75, -71.32, -66.89, -62.46, -58.03, -53.6, -49.17, -44.74, -40.31], \"selected_tick\": 5, \"session_id\": 77777, \"step_count\": 0\n}}"
+
+            resp = client.post(f"/api/audio", data = payload, content_type='application/json')
+            self.assertEqual(resp.status_code, 200)
+
+
+            # Tests 'Selected Tick' & 'Step Count' parameters
+            # BEFORE
+            resp = client.get('/api/audio/session/77777', follow_redirects=True)
+            self.assertEqual(resp.status_code, 200)
+            html = resp.get_data(as_text=True)
+            self.assertIn('Selected Tick: 5', html)
+            self.assertIn('Step Count: 0', html)
+
+            # AFTER
+            resp = client.patch("/api/audio/update/77777?step_count=7&selected_tick=7", follow_redirects=True)
+            self.assertEqual(resp.status_code, 200)
+            html = resp.get_data(as_text=True)
+            self.assertIn(f"Selected Tick: 7", html)
+            self.assertIn(f"Step Count: 7", html)
+
+            resp = client.patch('/api/audio/update/77777?ticks=-11.11,%20-11.11,%20-11.11,%20-11.11,%20-11.11,%20-11.11,%20-11.11,%20-11.11,%20-11.11,%20-11.11,%20-11.11,%20-11.11,%20-11.11,%20-11.11,%20-11.11', follow_redirects=True)
+            self.assertEqual(resp.status_code, 200)
+            html = resp.get_data(as_text=True)
+            self.assertIn(f"Ticks: [-11", html)
